82c3cf2b51b6764a20b17fc5739e9467b3e9cf4c
d r reniers <deveron-reniers@hotmail.com>
2020-10-09 15:59:26 +0200

    Update app.blade.php

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -58,7 +58,7 @@
         <!-- End Hotjar Analytics -->
     @endif
 
-    @if(getSetting('name="google_analytics_api_key"'))
+    @if(getSetting('google_analytics_api_key'))
         <!-- Google Analytics -->
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -66,7 +66,7 @@
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-XXXXX-Y', 'auto');
+            ga('create', '{!! getSetting('google_analytics_api_key') !!}', 'auto');
             ga('send', 'pageview');
         </script>
         <!-- End Google Analytics -->
