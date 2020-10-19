b23d68f9734bf89f207e28492079524b251141a9
mediaverse-nl <Overrated13@>
2020-10-19 04:02:54 +0200

    sitemap

--- app/Http/Controllers/Site/SiteMapController.php
+++ app/Http/Controllers/Site/SiteMapController.php
@@ -6,6 +6,7 @@ use App\Page;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Psr\Http\Message\UriInterface;
 use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 
@@ -13,10 +14,26 @@ class SiteMapController extends Controller
 {
     public function __invoke()
     {
-        $sitemap = SitemapGenerator::create(url('/'))
-            ->getSitemap(url());
-
         $pages = Page::select(['slug', 'updated_at'])->get();
+
+        $sitemap = SitemapGenerator::create(url('/'))
+            ->getSitemap(url())
+            ->shouldCrawl(function (UriInterface $url) use ($pages) {
+                // All pages will be crawled, except the contact page.
+                // Links present on the contact page won't be added to the
+                // sitemap unless they are present on a crawlable page.
+
+                foreach ($pages as $page) {
+                    return strpos($url->getPath(), $page->slug) === false;
+                }
+            });
+
+//        ->hasCrawled(function (Url $url) {
+//        # Ignore if URL is not canonical
+//        if(strpos($url->url, '?') !== false){
+//            return;
+//        }
+//    }
 
         foreach ($pages as $page) {
             $sitemap->add(Url::create('/test/' . $page->slug)
