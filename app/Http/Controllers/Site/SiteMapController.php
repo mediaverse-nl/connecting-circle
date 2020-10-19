07dc8cbdbad743ea25719330da3613a7b02db1a2
mediaverse-nl <Overrated13@>
2020-10-19 03:04:08 +0200

    seo editor added seo tabs

--- app/Http/Controllers/Site/SiteMapController.php
+++ app/Http/Controllers/Site/SiteMapController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Page;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Spatie\Sitemap\SitemapGenerator;
+
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class SiteMapController extends Controller
 {
     public function __invoke()
     {
-        SitemapGenerator::create(env('APP_URL'))
-            ->writeToFile(public_path('sitemap.xml'), 'sitemap.xml');
+        $sitemap = Sitemap::create();
+
+        $pages = Page::select(['slug', 'updated_at'])->get();
+
+        foreach ($pages as $page) {
+            $sitemap->add(Url::create('' . $page->slug)
+                ->setLastModificationDate($page->updated_at));
+        }
+
+        $sitemap->writeToFile(public_path('/sitemap.xml'));
 
         return 'site map aangemaakt';
     }
