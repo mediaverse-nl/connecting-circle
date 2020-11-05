75b5473818f87ce0ca940205e55f19a6bc1462b1
mediaverse-nl <Overrated13@>
2020-11-05 22:08:56 +0100

    admin candidate enabled lfm on page

--- resources/views/admin/pdf/cv.blade.php
+++ resources/views/admin/pdf/cv.blade.php
@@ -186,7 +186,7 @@
                 <table class="w-100" style="margin-bottom: 25px;">
                     @foreach($data->skills as $skill)
                         <tr style="">
-                            <td class="text-black-50" style="font-weight: bold; display: block; width: 70%; padding:7px 0px;">
+                            <td class="text-black-50" style="font-weight: 500; display: block; width: 70%; padding:7px 0px;">
                                 {!! nl2br($skill->vaardigheid) !!}
                             </td>
                             <td class="text-black-50" style="display: block; text-align: right">
